e81362cce87fc4c5f685eb0df54e3ee535b35245
hectorm1995 <35826251+hectorm1995@users.noreply.github.com>
2023-06-27 10:23:43 -0500

    Create Cambiar IP y Puerto DSpace.m

---
+++ Cambiar IP y Puerto DSpace.m
@@ -0,0 +1,23 @@
+# Ingresamos al directorio de instalación de DSpace:
+cd /dspace/config/
+
+# Ingresamos al archivo de configuración
+sudo nano local.cfg
+
+# Se cambia la direcciòn IP como se muestra en la figura Anexo 1
+
+# Ingresamos al siguiente directorio
+cd /home/tics/dspace-angular-dspace-7.5/config
+
+# Ingresamos al archivo de configuracion y cambiamos la IP (Anexo 2)
+sudo nano config.prod.yml
+
+# Reiniciamos tomcat
+sudo systemctl restart tomcat9.service
+
+# Reiniciamos el servicio pm2 
+cd /home/tics/dspace-angular-dspace-7.5
+pwd
+pm2 start dspace-ui.json
+
+
